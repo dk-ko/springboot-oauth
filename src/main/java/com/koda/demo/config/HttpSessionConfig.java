4cd7da17b14515c6f92426fb890f20bf5d212952
koda <kdkyung57@gmail.com>
2018-12-22 04:36:19 +0900

    feat: session setting

---
+++ src/main/java/com/koda/demo/config/HttpSessionConfig.java
@@ -0,0 +1,8 @@
+package com.koda.demo.config;
+
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+
+}
