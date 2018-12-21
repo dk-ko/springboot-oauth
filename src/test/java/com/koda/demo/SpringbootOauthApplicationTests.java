0ff6d0a4b0eb0058e383a43067a85e90e769474f
koda <kdkyung57@gmail.com>
2018-12-22 03:12:35 +0900

    chore: Set project

---
+++ src/test/java/com/koda/demo/SpringbootOauthApplicationTests.java
@@ -0,0 +1,17 @@
+package com.koda.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
