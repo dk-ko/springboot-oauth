b030fd6dbf6376345c58ea55d89afa8625a340a7
koda <kdkyung57@gmail.com>
2018-12-22 03:51:47 +0900

    feat: set google oauth

--- src/main/java/com/koda/demo/Application.java
+++ src/main/java/com/koda/demo/Application.java
@@ -1,13 +1,18 @@
 package com.koda.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
+	private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+			.properties(PROPERTIES)
+			.run(args);
+		
 	}
 
 }
